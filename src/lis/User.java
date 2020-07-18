0eb5cd50a576acbfac8d7b55f30fa8fa9fd2825d
guochunsheng <itxiaoguo214@163.com>
2020-07-18 22:05:26 +0800

    1

--- src/lis/User.java
+++ src/lis/User.java
@@ -2,22 +2,51 @@ package lis;
 
 public class User {
 
+	private String name;
+	private int age;
+	private double chengji;
+	
+	
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public int getAge() {
+		return age;
+	}
+
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+
+	public double getChengji() {
+		return chengji;
+	}
+
+
+	public void setChengji(double chengji) {
+		this.chengji = chengji;
+	}
+
+
+	public void addmain(String name,int age, double chengji){
+		
+		System.out.println("大家好，我是"+name+"，今年"+age+"岁,"+"我英语是 "+chengji+"分。");
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		User user = new User();
+        user.addmain("张三", 21, 22.78);
 	}
 
 }
 
-大家好
-
-
-我们都好
-public class User {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
